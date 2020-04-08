79c7fc66d559084125eda350736a6569ae5f7050
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-08 14:37:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,8 +5,8 @@ def square_array(array)
   new_array = []
   while array.length < counter do
     new_array.push(array[counter]**2)
-    counter += 1
     binding.pry
+    counter += 1
   end
   return new_array
 end

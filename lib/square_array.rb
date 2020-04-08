dd057ced06fc11da1be907c8250d7ec005e2f32a
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-08 14:25:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   counter = 0 
+  new_array = []
   while array[counter] do
-    math.sqrt(array[counter])
+    new_array << math.sqrt(array[counter])
     counter += 1
   end
 end

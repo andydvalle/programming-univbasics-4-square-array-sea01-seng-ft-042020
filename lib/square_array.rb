4d67ce00344cc41ac197c120a4ea7822a17328ab
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-08 14:39:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0 
   new_array = []
   binding.pry
-  while array.length < counter do
+  while array[counter] do
     new_array.push(array[counter]**2)
     counter += 1
   end
